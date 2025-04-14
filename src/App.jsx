a042698a809cdaceef9c8e7002cd10f1e64859c5
Moksleivis <Moksleivis@kitm.local>
2025-04-14 14:29:14 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import Dropdown from './Components/Dropdown'
-import Navbar from './Components/Navbar'
-import Hero from './Components/Hero'
-import Footer from './Components/Footer'
 
 function App() {
-  
+  useState
   return (
     <>
-      <Navbar/>
-      <Hero />
-
-      <h1 className='First'>HELLO</h1>
-
-      <Dropdown title='TITLE' content='LABA RYTA' />
-      <Dropdown title='Title' content='Labas rytas' />
-    
-      <div className="collapse bg-base-100 border-base-300 border">
-  <input type="checkbox" />
-  <div className="collapse-title font-semibold">How do I create an account?</div>
-  <div className="collapse-content text-sm">
-  Click the "Sign Up" button in the top right corner and follow the registration process.
-  </div>
-</div>
-
-      <Footer />
     </>
   )
 }
